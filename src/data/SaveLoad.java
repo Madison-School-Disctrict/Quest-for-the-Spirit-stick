af9b49b8aa3c43b4858cec3c25a83a6b98c1d520
Matthew McWilliams <matthewmcw5@gmail.com>
2025-05-14 10:28:39 -0600

    moved player save data from "src/data/saves/" to "savedata/". This is better for compiling the project as a jar.

--- src/data/SaveLoad.java
+++ src/data/SaveLoad.java
@@ -8,170 +8,164 @@ import java.io.ObjectOutputStream;
 import main.GamePanel;
 
 public class SaveLoad {
-	GamePanel gp;
-	public SaveLoad(GamePanel gp) {
-		this.gp = gp;
-		
-	}
-	
-	public void save(String name) {
-		try {
-			String fileName = "src/data/saves/" + name + "_save.dat";
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(fileName)));
-			DataStorage ds = new DataStorage();
-			ds.level = gp.player.level;
-			ds.maxLife = gp.player.maxLife;
-			ds.life = gp.player.life;
-			ds.maxMana = gp.player.maxMana;
-			ds.mana = gp.player.mana;
-			ds.strength = gp.player.strength;
-			ds.dexterity = gp.player.dexterity;
-			ds.exp = gp.player.exp;
-			ds.nextLevelExp = gp.player.nextLevelExp;
-			ds.coin = gp.player.coin;	
 
-			//Player location
-			ds.currentMap = gp.currentMap;
-			ds.currentWorldX = gp.player.worldX;
-			ds.currentWorldY = gp.player.worldY;
-			
-			
-			
-			//Player Inventory
-			for(int i = 0 ; i < gp.player.inventory.size(); i++) {
-				ds.itemNames.add(gp.player.inventory.get(i).name);
-				ds.itemAmounts.add(gp.player.inventory.get(i).amount);
-			}
-			
-			//Player Equipment
-			ds.currentWeaponSlot = gp.player.getCurrentWeaponSlot();
-			ds.currentShieldSlot = gp.player.getCurrentShieldSlot();
-			ds.currentMagicSlot = gp.player.getCurrentMagicSlot();
-			
-			//Objects on Map
-			
-			ds.mapObjectNames = new String[gp.maxMap][gp.obj[1].length];
-			ds.mapObjectWorldX = new int[gp.maxMap][gp.obj[1].length];
-			ds.mapObjectWorldY = new int[gp.maxMap][gp.obj[1].length];
-			ds.mapObjectLootNames = new String[gp.maxMap][gp.obj[1].length];
-			ds.mapObjectOpened = new boolean[gp.maxMap][gp.obj[1].length];
-			
-			for(int mapNum = 0 ; mapNum < gp.maxMap; mapNum++) {
-				for (int i = 0 ; i < gp.obj[1].length;i++) {
-					if(gp.obj[mapNum][i] == null) {
-						ds.mapObjectNames[mapNum][i] ="NA";
-					}
-					else {
-						ds.mapObjectNames[mapNum][i] = gp.obj[mapNum][i].name;
-						ds.mapObjectWorldX[mapNum][i] = gp.obj[mapNum][i].worldX;
-						ds.mapObjectWorldY[mapNum][i] = gp.obj[mapNum][i].worldY;
-						if(gp.obj[mapNum][i].loot != null) {
-							ds.mapObjectLootNames[mapNum][i] = gp.obj[mapNum][i].loot.name;
-						}
-						ds.mapObjectOpened[mapNum][i] = gp.obj[mapNum][i].opened;
-					}
-				}
-			}
-			
-			//Write the DataStorage object to the oos
-			oos.writeObject(ds);
-			oos.close();
-			System.out.println("Save Complete!");
-		}  catch (Exception e) {
-			System.out.println("Save Exception!");
-			e.printStackTrace();
-		}
-	}
-	public void load(String name) {
-		try {
-			String fileName = "src/data/saves/" + name + "_save.dat";
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(fileName)));
-			//Read the DataStorage object
-			DataStorage ds = (DataStorage)ois.readObject();
-			
-			gp.player.level = ds.level;
-			gp.player.maxLife = ds.maxLife;
-			gp.player.life = ds.life;
-			gp.player.maxMana = ds.maxMana;
-			gp.player.mana = ds.mana;
-			gp.player.strength = ds.strength;
-			gp.player.dexterity = ds.dexterity;
-			gp.player.exp = ds.exp;
-			gp.player.nextLevelExp = ds.nextLevelExp;
-			gp.player.coin = ds.coin;
+    GamePanel gp;
 
-			//Player location
-			gp.currentMap = ds.currentMap;
-			gp.player.worldX = ds.currentWorldX;
-			gp.player.worldY = ds.currentWorldY;
-			
-			//Player Inventory
-			gp.player.inventory.clear();
-			for (int i = 0; i< ds.itemNames.size(); i++ ) {
-				gp.player.inventory.add(gp.eGenerator.getObject(ds.itemNames.get(i)));
-				gp.player.inventory.get(i).amount = ds.itemAmounts.get(i);
+    public SaveLoad(GamePanel gp) {
+        this.gp = gp;
 
-		}
-			//Player Equipment
-			gp.player.currentWeapon = gp.player.inventory.get(ds.currentWeaponSlot);
-			gp.player.currentShield = gp.player.inventory.get(ds.currentShieldSlot);
-			gp.player.currentMagic = gp.player.inventory.get(ds.currentMagicSlot);
-			gp.player.getAttack();
-			gp.player.getDefense();
-			gp.player.getAttackImage();
-			
-			// Objects on map
-			
+    }
 
-			
-			for(int mapNum = 0; mapNum < gp.maxMap; mapNum++) {
-				for(int i = 0 ; i < gp.obj[1].length; i++) {
-					if(ds.mapObjectNames[mapNum][i].equals("NA")) {
-						gp.obj[mapNum][i] = null;
-					} else {
-						gp.obj[mapNum][i] = gp.eGenerator.getObject(ds.mapObjectNames[mapNum][i]);
-						gp.obj[mapNum][i].worldX = ds.mapObjectWorldX[mapNum][i];
-						gp.obj[mapNum][i].worldY = ds.mapObjectWorldY[mapNum][i];
-						if(ds.mapObjectLootNames[mapNum][i] != null) {
-							gp.obj[mapNum][i].setLoot(gp.eGenerator.getObject(ds.mapObjectLootNames[mapNum][i]));
-						}
-						gp.obj[mapNum][i].opened = ds.mapObjectOpened[mapNum][i];
-						if(gp.obj[mapNum][i].opened) {
-							gp.obj[mapNum][i].down1 = gp.obj[mapNum][i].image2;
-						}
-					}
-				}
-			}	
-			ois.close();
-			System.out.println("Load Complete!");
-		}catch (NullPointerException e) {
-			System.out.println("Load Exception.  It did not load correctly");
-			e.printStackTrace();
-		}catch (ClassNotFoundException e) {
-			System.out.println("Load Exception.  It did not load correctly");
-			e.printStackTrace();
-		}catch (java.io.EOFException e) {
-			System.out.println("Load Exception.  It did not load correctly");
-			e.printStackTrace();		
-		}catch(Exception e) {
-			System.out.println("Load Exception.  It did not load correctly");
-			e.printStackTrace();
-			
-		}
-		
-		
-	
-	}
+    public void save(String name) {
+        try {
+            String fileName = "savedata/" + name + "_save.dat";
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(fileName)));
+            DataStorage ds = new DataStorage();
+            ds.level = gp.player.level;
+            ds.maxLife = gp.player.maxLife;
+            ds.life = gp.player.life;
+            ds.maxMana = gp.player.maxMana;
+            ds.mana = gp.player.mana;
+            ds.strength = gp.player.strength;
+            ds.dexterity = gp.player.dexterity;
+            ds.exp = gp.player.exp;
+            ds.nextLevelExp = gp.player.nextLevelExp;
+            ds.coin = gp.player.coin;
 
-	public void deleteSave(String username) {
-		load(gp.usernameInput);
-		File file = new File( "src/data/saves/" + username + "_save.dat");
-		if (file.exists()) {
-			if (file.delete()) {
-				gp.loginMessage = ("Save data for " + username + " deleted.");
-			} else {
-				gp.loginMessage = ("Failed to delete save data for " + username + ".");
-			}
-		}
-	}
+            //Player location
+            ds.currentMap = gp.currentMap;
+            ds.currentWorldX = gp.player.worldX;
+            ds.currentWorldY = gp.player.worldY;
+
+            //Player Inventory
+            for (int i = 0; i < gp.player.inventory.size(); i++) {
+                ds.itemNames.add(gp.player.inventory.get(i).name);
+                ds.itemAmounts.add(gp.player.inventory.get(i).amount);
+            }
+
+            //Player Equipment
+            ds.currentWeaponSlot = gp.player.getCurrentWeaponSlot();
+            ds.currentShieldSlot = gp.player.getCurrentShieldSlot();
+            ds.currentMagicSlot = gp.player.getCurrentMagicSlot();
+
+            //Objects on Map
+            ds.mapObjectNames = new String[gp.maxMap][gp.obj[1].length];
+            ds.mapObjectWorldX = new int[gp.maxMap][gp.obj[1].length];
+            ds.mapObjectWorldY = new int[gp.maxMap][gp.obj[1].length];
+            ds.mapObjectLootNames = new String[gp.maxMap][gp.obj[1].length];
+            ds.mapObjectOpened = new boolean[gp.maxMap][gp.obj[1].length];
+
+            for (int mapNum = 0; mapNum < gp.maxMap; mapNum++) {
+                for (int i = 0; i < gp.obj[1].length; i++) {
+                    if (gp.obj[mapNum][i] == null) {
+                        ds.mapObjectNames[mapNum][i] = "NA";
+                    } else {
+                        ds.mapObjectNames[mapNum][i] = gp.obj[mapNum][i].name;
+                        ds.mapObjectWorldX[mapNum][i] = gp.obj[mapNum][i].worldX;
+                        ds.mapObjectWorldY[mapNum][i] = gp.obj[mapNum][i].worldY;
+                        if (gp.obj[mapNum][i].loot != null) {
+                            ds.mapObjectLootNames[mapNum][i] = gp.obj[mapNum][i].loot.name;
+                        }
+                        ds.mapObjectOpened[mapNum][i] = gp.obj[mapNum][i].opened;
+                    }
+                }
+            }
+
+            //Write the DataStorage object to the oos
+            oos.writeObject(ds);
+            oos.close();
+            System.out.println("Save Complete!");
+        } catch (Exception e) {
+            System.out.println("Save Exception!");
+            e.printStackTrace();
+        }
+    }
+
+    public void load(String name) {
+        try {
+            String fileName = "savedata/" + name + "_save.dat";
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(fileName)));
+            //Read the DataStorage object
+            DataStorage ds = (DataStorage) ois.readObject();
+
+            gp.player.level = ds.level;
+            gp.player.maxLife = ds.maxLife;
+            gp.player.life = ds.life;
+            gp.player.maxMana = ds.maxMana;
+            gp.player.mana = ds.mana;
+            gp.player.strength = ds.strength;
+            gp.player.dexterity = ds.dexterity;
+            gp.player.exp = ds.exp;
+            gp.player.nextLevelExp = ds.nextLevelExp;
+            gp.player.coin = ds.coin;
+
+            //Player location
+            gp.currentMap = ds.currentMap;
+            gp.player.worldX = ds.currentWorldX;
+            gp.player.worldY = ds.currentWorldY;
+
+            //Player Inventory
+            gp.player.inventory.clear();
+            for (int i = 0; i < ds.itemNames.size(); i++) {
+                gp.player.inventory.add(gp.eGenerator.getObject(ds.itemNames.get(i)));
+                gp.player.inventory.get(i).amount = ds.itemAmounts.get(i);
+
+            }
+            //Player Equipment
+            gp.player.currentWeapon = gp.player.inventory.get(ds.currentWeaponSlot);
+            gp.player.currentShield = gp.player.inventory.get(ds.currentShieldSlot);
+            gp.player.currentMagic = gp.player.inventory.get(ds.currentMagicSlot);
+            gp.player.getAttack();
+            gp.player.getDefense();
+            gp.player.getAttackImage();
+
+            // Objects on map
+            for (int mapNum = 0; mapNum < gp.maxMap; mapNum++) {
+                for (int i = 0; i < gp.obj[1].length; i++) {
+                    if (ds.mapObjectNames[mapNum][i].equals("NA")) {
+                        gp.obj[mapNum][i] = null;
+                    } else {
+                        gp.obj[mapNum][i] = gp.eGenerator.getObject(ds.mapObjectNames[mapNum][i]);
+                        gp.obj[mapNum][i].worldX = ds.mapObjectWorldX[mapNum][i];
+                        gp.obj[mapNum][i].worldY = ds.mapObjectWorldY[mapNum][i];
+                        if (ds.mapObjectLootNames[mapNum][i] != null) {
+                            gp.obj[mapNum][i].setLoot(gp.eGenerator.getObject(ds.mapObjectLootNames[mapNum][i]));
+                        }
+                        gp.obj[mapNum][i].opened = ds.mapObjectOpened[mapNum][i];
+                        if (gp.obj[mapNum][i].opened) {
+                            gp.obj[mapNum][i].down1 = gp.obj[mapNum][i].image2;
+                        }
+                    }
+                }
+            }
+            ois.close();
+            System.out.println("Load Complete!");
+        } catch (NullPointerException e) {
+            System.out.println("Load Exception.  It did not load correctly");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Load Exception.  It did not load correctly");
+            e.printStackTrace();
+        } catch (java.io.EOFException e) {
+            System.out.println("Load Exception.  It did not load correctly");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Load Exception.  It did not load correctly");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public void deleteSave(String username) {
+        load(gp.usernameInput);
+        File file = new File("savedata/" + username + "_save.dat");
+        if (file.exists()) {
+            if (file.delete()) {
+                gp.loginMessage = ("Save data for " + username + " deleted.");
+            } else {
+                gp.loginMessage = ("Failed to delete save data for " + username + ".");
+            }
+        }
+    }
 }
